f1144b994f83cab06bbc80c6cece174674f19939
connorGA <ronnoccap@gmail.com>
2022-12-10 15:24:33 -0800

    feat: add templates to base app, home page links to room pages by pk

--- base/views.py
+++ base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+rooms = [
+    {'id':1, 'name': 'Working on project'},
+    {'id':2, 'name': 'world cup chat'},
+    {'id':3, 'name': 'python practice'},
+
+]
+
+
+def home(request):
+    context = { 'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
