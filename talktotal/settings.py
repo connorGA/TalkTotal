2ba70a0bcd2c977611dcfc541572369ade11a79c
connorGA <ronnoccap@gmail.com>
2022-12-10 14:28:20 -0800

    fix: typo in settings.py, app now connected properly

--- talktotal/settings.py
+++ talktotal/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
